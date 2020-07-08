36ef847fd1ecdc81291ab7e366c67c8ef4013489
fifabell@naver.com <fifabell@naver.com>
2020-07-08 14:34:07 +0900

    Update step12_LIS.py

--- real_/step12_LIS.py
+++ real_/step12_LIS.py
@@ -7,19 +7,40 @@
 # output
 # 4
 
-totalNum = int(input())
-arr_check = [0 for i in range(totalNum)]
-arr = list(map(int,input().split()))
-# print(arr_check)
+# totalNum = int(input())
+# arr = list(map(int,input().split()))
+# def lis(arr):
+#     if not arr:
+#         return 0
+    
+#     ret = 1
+#     for i in range(len(arr)):
+#         i
+#         nxt = []
+#         for j in range(i+1, len(arr)):
+#             if arr[i] < arr[j]:
+#                 nxt.append(arr[j])
+#             j
+#         ret = max(ret, 1 + lis(nxt))
+#     return ret	
 
-for i in range(totalNum-1):
-    arr_check[0] = arr[0]
-    arr_check[i+1] = arr[i+1] - arr[i]
-    # if arr[i] < arr[i+1]:
-    #     max_num = max_num + 1
-        # arr_check[i+1] = arr_check[i] + add_val
-    # print(arr[i])
-    # print(arr[i+1])
-    # print("------")
-# print(arr)
-print(arr_check)
+# print(lis(arr))
+
+totalNum = int(input())
+arr = list(map(int,input().split()))
+dp = [0 for i in range(totalNum)]
+i_max = 0
+for i in range(totalNum):
+    for j in range(i):
+        # print("i="+str(i)+", j="+str(j))
+        # print("arr[i]= "+str(arr[i])+", arr[j]= "+str(arr[j]))
+        # print("dp[i]= "+str(dp[i])+", dp[j]= "+str(dp[j]))
+        # print(dp)
+        if arr[i] > arr[j] and dp[i] < dp[j]:
+            dp[i] = dp[j]
+        # print(dp)
+        
+    # print("--------")
+    dp[i] += 1
+ 
+print(max(dp))
